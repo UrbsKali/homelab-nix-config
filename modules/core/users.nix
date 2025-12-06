ffb62bfd188f48d932f5ae4603cf16abb374c6dc
UrbsKali <urbain.lantres@tutanota.com>
2025-12-06 18:26:23 +0100

    give up on storing the ssh keys on sops for centralisation

--- modules/core/users.nix
+++ modules/core/users.nix
@@ -9,13 +9,11 @@
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [];
     hashedPasswordFile = config.sops.secrets."user_passwords/urbai".path;
-    openssh.authorizedKeys.keyFiles = [
-      config.sops.secrets."ssh_keys/0".path
+    openssh.authorizedKeys.keys = [
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQD5ADKy6laNmJTEyOfBLJCp7OmKjniZfxTwQTy22ydlJFfLA0fbfIFbEV2qi1PdDaJ5IGZS3YCliiUP8d5OjjX/fSW978WRuAEP32DTUlCw6XKtfz1sCyDH2uam039fmJVkp57KpkhBXVNhUsS2qnstkIW1/e7m4JlXcvo0Xwv/GSWf1zuqaFcby7hNERfX+d1j3XZGYwP9ItYoe7ijvtgwGUYQbrWJSkW1Sd03aOLEKyptPdFJa+y+DK33gQFGeDMXGRS+XwuMo5V+/9ky/iUMt2DZh3ydzfDzrfb73EB3rBjgXGha8pLuntyYCH1rQOlZC9ehC8YcazrY+Kp1h1EHCci+3qvOUrkWCztnoK9IaDqPXqkGyv8gxSkV04uwOoKpTywYj8VQCSkicHpffVZneUialy/nY9ZXXJzDxUhcm6UhTUBU8dNBBS0AKoiOwqFIxu+PxYD/iQCHg5a9adgeZ5lbUld9FW1AiF1+DuPqujRRzQTRyDqwHE9KIutFyXU= urbai@beepboop"
     ];
+
     shell = pkgs.zsh;
   };
-
-  sops.secrets."ssh_keys/0" = {
-    neededForUsers = true;
-  };
 }
+
