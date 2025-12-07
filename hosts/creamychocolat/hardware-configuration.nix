ee61074e978b3cf963edd53a044c9927a994635b
UrbsKali <urbain.lantres@tutanota.com>
2025-12-07 22:53:11 +0100

    Update HW config for creamychocolat

--- hosts/creamychocolat/hardware-configuration.nix
+++ hosts/creamychocolat/hardware-configuration.nix
@@ -8,23 +8,19 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/5625ab34-4c4e-4948-a0c3-00b0c874c2b4";
+    { device = "/dev/disk/by-uuid/ad2f8155-4930-4b0c-b427-249d37a32f16";
       fsType = "ext4";
     };
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/7CAC-7FAD";
-      fsType = "vfat";
-      options = [ "fmask=0022" "dmask=0022" ];
-    };
-
-  swapDevices = [ ];
+  swapDevices =
+    [ { device = "/dev/disk/by-uuid/872c433f-a440-4aea-bf66-b03bcd88e178"; }
+    ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
