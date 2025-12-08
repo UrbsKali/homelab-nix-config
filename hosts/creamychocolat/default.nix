2b05a415eb7623891c4d40afd31f8cb33db1525b
UrbsKali <urbain.lantres@tutanota.com>
2025-12-08 17:00:05 +0100

    add UEFI support for creamychocolat

--- hosts/creamychocolat/default.nix
+++ hosts/creamychocolat/default.nix
@@ -18,6 +18,8 @@
 
   # Bootloader
   boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.device = "nodev";
+  boot.loader.grub.efiSupport = true;
   boot.loader.grub.useOSProber = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 }
